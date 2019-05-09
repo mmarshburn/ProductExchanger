ad1865c93137efea6066e46cc0b839bae9e1916f
Matteo <mmarshburn@smu.edu>
2019-05-08 21:17:46 -0500

    917

--- slapshotz/src/components/pages/Login.js
+++ slapshotz/src/components/pages/Login.js
@@ -1,50 +1,48 @@
-import React from "react";
-import LogInForm from "./LogInForm";
-import background from "./../images/hockey.jpg";
-import { Link } from "react-router-dom";
-import "./login.css";
+// import React from "react";
+// import { Link } from "react-router-dom";
 
 
 
-const Login = () => {
 
-  const style = {
-    container: {
-      minHeight: "100vh",
-      minWidth: "100%",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundImage: `url(${background})`,
-      backgroundSize: "cover",
-      backgroundRepeat: "no-repeat",
-      padding: "5rem",
-      textAlign: "center"
-    },
-    font: {
-      color: "tomato"
-    },
-    button: {
-      marginTop: "20px"
-    }
-  }
+// const Login = () => {
 
-  return (
-    <div style={style.container}>
-      <div style={style.font}>
-        <h1 className="landing-title">Welcome to the Slapshotz</h1>
-        <div style={{ backgroundColor: "#cfd1d6", padding: "20px" }}>
-          <LogInForm/>
-        </div>
-        <div style={style.button}>
-          <Link to="/signup" style={{ color:"tomato" }}> Sign Up</Link>
-        </div>
-        <div style={style.button}>
-          <Link to="/home" style={{ color:"tomato" }}> Continue as guest</Link>
-        </div>
-      </div>
-    </div>
-  );
-}
+//   const style = {
+//     container: {
+//       minHeight: "100vh",
+//       minWidth: "100%",
+//       display: "flex",
+//       alignItems: "center",
+//       justifyContent: "center",
+//       backgroundImage: `url(${background})`,
+//       backgroundSize: "cover",
+//       backgroundRepeat: "no-repeat",
+//       padding: "5rem",
+//       textAlign: "center"
+//     },
+//     font: {
+//       color: "tomato"
+//     },
+//     button: {
+//       marginTop: "20px"
+//     }
+//   }
 
-export default Login;
+//   return (
+//     <div style={style.container}>
+//       <div style={style.font}>
+//         <h1 className="landing-title">Welcome to the Slapshotz</h1>
+//         <div style={{ backgroundColor: "#cfd1d6", padding: "20px" }}>
+//           <LogInForm/>
+//         </div>
+//         <div style={style.button}>
+//           <Link to="/signup" style={{ color:"tomato" }}> Sign Up</Link>
+//         </div>
+//         <div style={style.button}>
+//           <Link to="/home" style={{ color:"tomato" }}> Continue as guest</Link>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Login;
